8d3d866c49ce4614099ccbdb5a020edc30144b3d
MohitNITTIN <mohitsaini25j@gmail.com>
2023-12-29 19:47:20 +0530

    testing0

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -22,7 +22,7 @@ const Header = () => {
         </form>
         <ul className="flex gap-4">
           <Link to="/home">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
+            <li className="hidde sm:inline text-slate-700 hover:underline">
               Home
             </li>
           </Link>
